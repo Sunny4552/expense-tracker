7f22e02b953c0dcaf88466499a61265a395d0d1e
Sunny <sunnymistry78@yahoo.com>
2020-12-10 00:49:13 -0800

    Update ExpenseTracker.java

--- expense-tracker/src/application/ExpenseTracker.java
+++ expense-tracker/src/application/ExpenseTracker.java
@@ -4,8 +4,21 @@ import java.util.ArrayList;
 
 public class ExpenseTracker {
 
+	private UserDb database;
 
+	public ExpenseTracker() {
+		database = new UserDb();
+	}
 
+	public void createNewUser(User user) {
+		// calls writeNewUser() from UserDb
+		database.addUser(user);
 
+		// create empty expenses
+	}
 
+	public void addExpenseDB(User current, String expense)
+	{
+		database.addExpense(current, expense);
+	}
 }
